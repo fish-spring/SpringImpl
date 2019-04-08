2c2e57ec426ae47d6ee39120e689adb58408798f
apple <wow@github.com>
2019-04-08 12:22:51 +0800

    getBean with constructor, set property, factory

---
+++ src/main/java/zoo/Creator.java
@@ -0,0 +1,7 @@
+package zoo;
+
+public class Creator {
+    public static Panda createChosenPanda(){
+        return new Panda("DaBao", 55.5f);
+    }
+}
--- src/test/java/zoo/PandaTest.java
+++ src/test/java/zoo/PandaTest.java
@@ -10,7 +10,6 @@ import static org.junit.Assert.*;
 
 public class PandaTest {
     private static ApplicationContext context;
-
     @BeforeClass
     public static void getContext(){
         context = new ClassPathXmlApplicationContext("spring.xml");
@@ -20,11 +19,22 @@ public class PandaTest {
     public void getBeanWithConstructor(){
         Panda HuanHuan = (Panda) context.getBean("HuanHuan");
         System.out.println(JSONObject.toJSONString(HuanHuan));
+        // {"name":"HuanHuan","weight":22.2}
+        Panda Huan = (Panda) context.getBean("HuanThePanda");
+        System.out.println(Huan == HuanHuan);  // true
     }
 
     @Test
     public void getBeanWithProperties(){
         Panda LeLe = (Panda) context.getBean("LeLe");
         System.out.println(JSONObject.toJSONString(LeLe));
+        // {"name":"LeLe","weight":33.14}
+    }
+
+    @Test
+    public void getBeanWithStaticFactory(){
+        Panda DaBao = (Panda) context.getBean("DaBao");
+        System.out.println(JSONObject.toJSONString(DaBao));
+        // {"name":"DaBao","weight":55.5}
     }
 }
